b2048f1f9d6723679efd1fc820fcfdb62548372a
Worna <etu4015@henallux.be>
2020-12-01 15:01:43 +0100

    labo 6 en cours

--- smartcity TBC/controleur/sportHallCustomerORM.js
+++ smartcity TBC/controleur/sportHallCustomerORM.js
@@ -3,7 +3,56 @@ const SportHallCustomerORM = require("../ORM/model/SportHallCustomer");
 const SportHallORM = require("../ORM/model/SportHall");
 const sequelize = require("../ORM/sequelize");
 const {Sequelize} = require("sequelize");
-
+/**
+ * @swagger
+ * components:
+ *  responses:
+ *      AddedCustomerToSportHall:
+ *          description: The customer has been added to the sport hall
+ *      SportHallDoesNotExist:
+ *          description: The sport hall doesn't exist
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          error:
+ *                              type: string
+ *      CustomerDoesNotExist:
+ *          description: The customer doesn't exist
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          error:
+ *                              type: string
+ *
+ *  requestBodies:
+ *      AddCustomerToSportHall:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          sportHall:
+ *                              type: object
+ *                              properties:
+ *                                  id:
+ *                                      type: integer
+ *                              required:
+ *                                  - id
+ *                          customer:
+ *                              type: object
+ *                              properties:
+ *                                  id:
+ *                                      type: integer
+ *                              required:
+ *                                  - id
+ *                      required:
+ *                          - sportHall
+ *                          - customer
+ */
 module.exports.insertSportHallCustomer = async (req, res) => {
     const {sportHall, customer} = req.body;
     try{
--- smartcity TBC/modele/customerDB.js
+++ smartcity TBC/modele/customerDB.js
@@ -1,7 +1,9 @@
+const {getHash} = require("../utils/utils");
+
 module.exports.createCustomer = async (client, firstName, lastName, birthDate, gender, phoneNumber, email, password, inscriptionDate, isManager, isInstructor, language) => {
     return await client.query(`
         INSERT INTO customer(first_name, last_name, birth_date, gender, phone_number, email, password, inscription_date, is_manager, is_instructor, language)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`, [firstName, lastName, birthDate, gender, phoneNumber, email, password, inscriptionDate, isManager, isInstructor, language]
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`, [firstName, lastName, birthDate, gender, phoneNumber, email, await getHash(password), inscriptionDate, isManager, isInstructor, language]
     );
 }
 module.exports.customerExist = async (client, idCustomer) => {
@@ -18,8 +20,62 @@ module.exports.getCustomer = async (client, email) => {
 }
 
 module.exports.updateCustomer = async (client, id, firstName, lastName, birthDate, gender, phoneNumber, email, password, inscriptionDate, isManager, isInstructor, language) => {
-   return await client.query(`
-        UPDATE customer SET first_name = $1, last_name = $2, birth_date = $3, gender = $4, phone_number = $5, email = $6, password = $7, inscription_date = $8, is_manager = $9, is_instructor = $10, language = $11
-        WHERE id = $12
-   `, [firstName, lastName, birthDate, gender, phoneNumber, email, password, inscriptionDate, isManager, isInstructor, language, id]);
-}
+    const params = [];
+    const querySet = [];
+    let query = "UPDATE customer SET ";
+    if(firstName !== undefined){
+        params.push(firstName);
+        querySet.push(` first_name = $${params.length} `);
+    }
+    if(lastName !== undefined){
+        params.push(lastName);
+        querySet.push(` last_name = $${params.length} `);
+    }
+    if(birthDate !== undefined){
+        params.push(birthDate);
+        querySet.push(` birth_date = $${params.length} `);
+    }
+    if(gender !== undefined){
+        params.push(gender);
+        querySet.push(` gender = $${params.length} `);
+    }
+    if(phoneNumber !== undefined){
+        params.push(phoneNumber);
+        querySet.push(` phone_number = $${params.length} `);
+    }
+    if(email !== undefined){
+        params.push(email);
+        querySet.push(` email = $${params.length} `);
+    }
+    if(password !== undefined){
+        params.push(await getHash(password));
+        querySet.push(` password = $${params.length} `);
+    }
+    if(inscriptionDate !== undefined){
+        params.push(inscriptionDate);
+        querySet.push(` inscription_date = $${params.length} `);
+    }
+    if(isManager !== undefined){
+        params.push(isManager);
+        querySet.push(` is_manager = $${params.length} `);
+    }
+    if(isInstructor !== undefined){
+        params.push(isInstructor);
+        querySet.push(` is_instructor = $${params.length} `);
+    }
+    if(language !== undefined){
+        params.push(language);
+        querySet.push(` language = $${params.length} `);
+    }
+
+    if(params.length > 0){
+        query += querySet.join(',');
+        params.push(id);
+        query += ` WHERE id = $${params.length}`;
+
+        return client.query(query, params);
+    } else {
+        throw new Error("No field to update");
+    }
+
+   }
